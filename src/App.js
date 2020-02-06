f267e8434123803bf10e9dd070760ab15df065b7
qasim <masterqasim95@gmail.com>
2020-02-06 18:06:19 +0500

    recuitment system

--- src/App.js
+++ src/App.js
@@ -1,26 +1,59 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component , Fragment } from 'react'
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Header from './layout/Header'
+import Footer from './layout/Footer'
+import SignUp from './components/SignUp'
+import SignIn from './components/SignIn'
+import Student from './components/student/Student'
+import Company from './components/company/Company'
+import * as firebase from "firebase";
+import './config.js'
+import Admin from './components/admin/Admin';
+import { Redirect } from 'react-router-dom'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+ class App extends Component {
+   state={
+     type:''
+   }
+  componentWillMount(){
+    let userUid = localStorage.getItem("userUid");
+      if(userUid){
+        firebase.database().ref('users/' + userUid).on("value",(data)=>{
+            let userData = data.val()
+          if(userData){
+            this.setState({type:userData.userType})
+          }
+        })   
+      }
+   }
+  render() {
+    console.log(this.state)
+    return (
+      <Router>
+        <Fragment>
+          <Header/>
+          {this.state.type==="student"?
+           <Redirect to='/Student' />
+            :  this.state.type==="company"?
+            <Redirect to='/Company' />
+            : this.state.type==="admin"?
+           
+            <Redirect to='/Admin' />
+            :
+            <Redirect to='/SignIn' />
+          }
+          
+          <Route path="/SignUp" component={SignUp}></Route> 
+          <Route path="/SignIn" component={SignIn}/>
+          <Route path="/Student" component={Student}/>
+          <Route path="/Company" component={Company}/>
+          <Route path="/Admin" component={Admin}/>
+         
+        </Fragment>
+      </Router>
+      
+    )
+  }
 }
-
 export default App;
